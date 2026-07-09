b4f3e96f2d6ca6b2ee7369748944bb080d0e6729
MyName <mymail@example.com>
2026-07-10 02:00:26 +0300

    Remove config.py and licenses.db from repository, add to gitignore

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-ADMIN_KEY = os.environ.get("ADMIN_KEY", "change_me")
-ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change_me")
-SECRET_KEY = os.environ.get("SECRET_KEY", "change_me")
